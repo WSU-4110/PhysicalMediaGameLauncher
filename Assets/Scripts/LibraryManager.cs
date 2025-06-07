04838a129dcaf97d738c2be95ef3f27a6daafb45
Ali Hussain <alihussain2001@gmail.com>
2025-06-07 19:08:02 -0400

    Finish first round of library and physical media managers

---
+++ Assets/Scripts/LibraryManager.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+
+[Serializable]
+public class Game
+{
+    public string gameName;
+    public string gamePath;
+    public string gameDrive;
+    public bool isAvaliable;
+
+    public string GetFullPath()
+    {
+        return System.IO.Path.Join(gameDrive, gamePath);
+    }
+}
+
+public class LibraryManager : MonoBehaviour
+{
+    public static LibraryManager instance { get; private set; } = null;
+    private static string libraryCachePath = System.IO.Path.Join(Application.persistentDataPath, "library.json");
+
+    public Dictionary<string, Game> games = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        LoadCache();
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void LoadCache()
+    {
+        try
+        {
+            string json = File.ReadAllText(libraryCachePath);
+            games = JsonUtility.FromJson<Dictionary<string, Game>>(json);
+            foreach (Game game in games.Values) {
+                game.isAvaliable = File.Exists(game.GetFullPath());
+            }
+        }
+        catch (System.Exception e)
+        {
+            games = new Dictionary<string, Game>();
+            Debug.LogError($"[LibraryManager] Error reading cache! Reason: {e.Message}");
+        }
+    }
+
+    private void SaveCache()
+    {
+        try
+        {
+            File.WriteAllText(libraryCachePath, JsonUtility.ToJson(games));
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"[LibraryManager] Error writing cache! Reason: {e.Message}");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveCache();
+    }
+
+    public void AddGameToLibrary(string id, Game game)
+    {
+        if (games.ContainsKey(id))
+            games[id] = game;
+        else
+            games.Add(id, game);
+    }
+
+    public void RemoveGameFromLibrary(string id, bool softDelete = true)
+    {
+        if (softDelete)
+            games[id].isAvaliable = false;
+        else
+            games.Remove(id);
+    }
+
+}
---
+++ Assets/Scripts/PhysicalMediaManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+class GameMetadata
+{
+    public string id;
+    public string gameName;
+    public string gameDrive;
+    public string gamePath;
+
+    public Game convertToGame()
+    {
+        return new Game
+        {
+            gameName = this.gameName,
+            gamePath = this.gamePath,
+            gameDrive = this.gameDrive,
+            isAvaliable = true,
+        };
+    }
+}
+
+
+public class PhysicalMediaManager : MonoBehaviour
+{
+    public static PhysicalMediaManager instance { get; private set; } = null;
+    public const string DRIVE_GAME_METADATA_FILENAME = "gameData.json";
+
+    public Dictionary<string, List<string>> driveToGameIDS = new Dictionary<string, List<string>>();
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ScanDriveForGames(string drivePath)
+    {
+        string driveGamePath = System.IO.Path.Join(drivePath, DRIVE_GAME_METADATA_FILENAME);
+
+        try
+        {
+            GameMetadata gameMetadata = JsonUtility.FromJson<GameMetadata>(driveGamePath);
+            if (LibraryManager.instance != null)
+                LibraryManager.instance.AddGameToLibrary(gameMetadata.id, gameMetadata.convertToGame());
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"[PhysicalMediaManager] Failed to scan drive for games! Reason: {e}");
+        }
+    }
+
+    public void RemoveGamesFromDrive(string drivePath)
+    {
+        foreach (string gameID in driveToGameIDS[drivePath])
+            if(LibraryManager.instance != null)
+                LibraryManager.instance.RemoveGameFromLibrary(gameID);
+    }
+
+
+}
