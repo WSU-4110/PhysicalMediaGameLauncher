9331186a8f927721c479116f7c5031a43c4cc13c
Ali Hussain <alihussain2001@gmail.com>
2025-06-07 18:29:01 -0400

    Device Listener Manager properly reports mounted and unmounted drives

--- Assets/Scripts/DeviceListenerManager.cs
+++ Assets/Scripts/DeviceListenerManager.cs
@@ -1,7 +1,3 @@
-#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-using System;
-using System.Runtime.InteropServices;
-#endif
 using System.Collections;
 using System.IO;
 using System.Linq;
@@ -12,8 +8,10 @@ using System.Collections.Generic;
 public class DeviceListenerManager : MonoBehaviour
 {
     public static DeviceListenerManager instance { get; private set; } = null;
-    private List<DriveInfo> currentlyInsertedDrives = new List<DriveInfo>();
-    private Dictionary<DriveInfo, List<string>> driveToPaths = new Dictionary<DriveInfo, List<string>>();
+    private List<string> currentlyInsertedDrives = new List<string>();
+    private Dictionary<string, List<string>> driveToPaths = new Dictionary<string, List<string>>();
+
+    public float interval = 2f;
 
     void Awake()
     {
@@ -28,20 +26,42 @@ public class DeviceListenerManager : MonoBehaviour
     }
 
 
-    public void MountedDrive()
+    public void MountedDrive(string drive)
     {
-        
+        // Fill the rest of this implementation when Physical Media Manager is ready
+        Debug.Log($"Mounted {drive}");
     }
 
-    public void UnmountedDrive()
+    public void UnmountedDrive(string drive)
     {
-        
+        // Fill the rest of this implementation when Physical Media Manager is ready
+        Debug.Log($"Unmounted {drive}");
     }
 
 
     public IEnumerator ScanForDevices()
     {
-        List<DriveInfo> scannedDrives = DriveInfo.GetDrives().Where(drive => drive.IsReady && drive.DriveType == DriveType.Removable).ToList();
-        yield return null;
+        while (true)
+        {
+            List<string> scannedDrives = DriveInfo.GetDrives().Where(drive => drive.IsReady && drive.DriveType == DriveType.Removable).Select(x => x.RootDirectory.ToString()).ToList();
+            for (int i = 0; i < currentlyInsertedDrives.Count; i++)
+            {
+                string drive = currentlyInsertedDrives[i];
+                // If a drive doesn't exist in the scannedDrives but does exist in the currentlyInsertedDrives, it's most likely been unmounted
+                if (!scannedDrives.Contains(drive))
+                    UnmountedDrive(drive);
+            }
+
+            for (int i = 0; i < scannedDrives.Count; i++)
+            {
+                string drive = scannedDrives[i];
+                // If a drive exists in scannedDrives but not in currentlyInsertedDrives, it's most likely a newly mounted drive
+                if (!currentlyInsertedDrives.Contains(drive))
+                    MountedDrive(drive);
+            }
+
+            currentlyInsertedDrives = scannedDrives;
+            yield return new WaitForSeconds(interval);
+        }
     }
 }
