a7e6e4d1876799cfa53207dedaefdec36677c898
Ali Hussain (Coolsonickirby) <alihussain2001@gmail.com>
2025-07-28 18:39:25 -0400

    Remove all unnecessary usings that spawned errors

--- Assets/Scripts/UILauncherManager.cs
+++ Assets/Scripts/UILauncherManager.cs
@@ -1,12 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.Collections;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using UnityEngine.WSA;
 
 
 public enum LauncherState
